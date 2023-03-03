450d9963df08c8057ae32df049d0748845c6290c
Sudhir Bhausaheb Pawar <sudhir_bhausaheb_pawar@epam.com>
2023-03-03 22:10:24 +0530

    call Now button added

--- js/customJs.js
+++ js/customJs.js
@@ -1,34 +1,31 @@
-// const contactForm = document.getElementById('contact-form');
+const contactForm = document.getElementById('contactForm');
 
-// contactForm.addEventListener('submit', function(e) {
-//   e.preventDefault(); 
-  // prevent the form from submitting normally
+contactForm.addEventListener('submit', function(e) {
+  e.preventDefault(); 
+//   prevent the form from submitting normally
   
-  // Get form data
-//   const name = document.getElementById('name').value;
-//   const contactNo = document.getElementById('contactNo').value;
-//   const startingPoint = document.getElementById('startingPoint').value;
-//   const endigPoint = document.getElementById('endingPoint').value;
-//   const date = document.getElementById('date').value;
+//   Get form data
+  const name = document.getElementById('name').value;
+  const contactNo = document.getElementById('contactNo').value;
+  const startingPoint = document.getElementById('startingPoint').value;
+  const endigPoint = document.getElementById('endingPoint').value;
+  const date = document.getElementById('date').value;
 
-//   const message = document.getElementById('message').value;
+  const message = document.getElementById('message').value;
   
-//   const url = "http://wa.me/+918275578157/?text="+encodeURIComponent(
-//     `
-//     Hi, I'm interested in renting a car from your business. Can you provide more information about your available cars and prices?\nMy name: ${name}\nContact No: ${contactNo}\nStarting Point: ${startingPoint}\nEnding Point: ${endingPoint}\nDate: ${date}\nMessage: ${Message}Thank you!
-//     `
-//   ) 
 
 
 
-//   window.location.href = url;
-// });
-// // Create mailto link
-// const subject = encodeURIComponent('Message from ' + name);
-// const body = encodeURIComponent(message);
-// const mailtoLink = 'mailto:' + email + '?subject=' + subject + '&body=' + body;
+// Create mailto link
+const subject = encodeURIComponent('Message from ' + name);
+const body = encodeURIComponent("Hi, I'm interested in renting a car from your business. Can you provide more information about your available cars and prices?\nName: "+name+'\nStarting City: '+startingPoint+'\nDestination: '+endigPoint+'\nDate: '+date+'\n'+ message);
+const mailtoLink = 'mailto:suryawanshis973.ss@gmail.com' + '?subject=' + subject + '&body=' + body;
 
-// // Open default email client
+// Open default email client
+//   window.location.href = mailtoLink;
+window.open(mailtoLink, '_blank');
+
+});
 
 
 
